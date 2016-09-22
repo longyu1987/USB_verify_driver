0729927ccb98f40faec89591e53ffcf0720d61f8
weitaowang <WeitaoWang@zhaoxin.com>
2016-09-22 20:51:55 +0800

    this is test for remote push
    Signed-off-by: weitaowang <WeitaoWang@zhaoxin.com>

--- linux-4.4.20/drivers/usb/host/xhci.c
+++ linux-4.4.20/drivers/usb/host/xhci.c
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
+//this is modify test for push
 #include <linux/pci.h>
 #include <linux/irq.h>
 #include <linux/log2.h>
