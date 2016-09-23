3723a8c164e6c6aaddd22757d2ad6273c2184168
dell <wwt8723@163.com>
2016-09-23 11:00:31 +0800

    this is mulitiply modify test by dell
    Signed-off-by: dell <wwt8723@163.com>

--- linux-4.4.20/drivers/usb/host/xhci.c
+++ linux-4.4.20/drivers/usb/host/xhci.c
@@ -19,6 +19,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+//this is mulitiply modify test by dell
 //this is modify test for push
 #include <linux/pci.h>
 #include <linux/irq.h>
